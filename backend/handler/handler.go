059d82dfeb5ad6d2ff17ddb2e8ad7669d01898ca
Tarek Ali <tarekali15@outlook.com>
2020-08-08 16:34:59 -0500

    boilerplate for funcs

---
+++ backend/handler/handler.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Users returns a json result of the all users in the User table
+func Users(c *gin.Context) {
+
+}
+
+// User returns a json result of a User's data (with desired UUID)
+func User(c *gin.Context) {
+
+}
+
+// Posts returns a json result of all the posts in the Post table
+func Posts(c *gin.Context) {
+
+}
+
+// Post returns a json result of a post by post_uuid
+func Post(c *gin.Context) {
+	
+}
