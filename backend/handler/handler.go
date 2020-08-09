4bcff2692b109ef534b353e1aed3fe1692139b97
Tarek Ali <tarekali15@outlook.com>
2020-08-08 20:09:39 -0500

    post handler

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"Awty-Portal/backend/dbutils"
+
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 )
 
 // Users returns a json result of the all users in the User table
@@ -19,10 +21,18 @@ func User(c *gin.Context) {
 
 // Posts returns a json result of all the posts in the Post table
 func Posts(c *gin.Context) {
-
+	posts := []dbutils.Post{}
+	dbutils.SelectPosts(posts)
+	c.JSON(200, posts)
 }
 
 // Post returns a json result of a post by post_uuid
 func Post(c *gin.Context) {
-	
+	uuidArg := c.Query("uuid")
+	post := dbutils.Post{}
+	conversion, err := uuid.FromString(uuidArg)
+	if err != nil {
+		panic(err)
+	}
+	dbutils.SelectPostsByUUID(conversion, &post)
 }
