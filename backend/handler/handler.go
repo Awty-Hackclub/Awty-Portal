3a5f53513328cc9b794c231a91fa1b2e0458860f
Tarek Ali <tarekali15@outlook.com>
2020-08-13 09:51:15 -0500

    implemented some internal protections that (in a sense) solve one of the issues in the repo

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"Awty-Portal/backend/dbutils"
+	"errors"
 
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
@@ -17,10 +18,13 @@ func Users(c *gin.Context) {
 // User returns a json result of a User's data (with desired UUID)
 func User(c *gin.Context) {
 	uuidArg := c.Query("uuid")
+	if uuidArg == "" {
+		c.AbortWithError(400, errors.New("You need to request with a uuid query string"))
+	}
 	user := dbutils.User{}
 	conversion, err := uuid.FromString(uuidArg)
 	if err != nil {
-		panic(err)
+		c.AbortWithError(400, errors.New("Invalid UUID"))
 	}
 	dbutils.SelectUserByUUID(conversion, &user)
 	c.JSON(200, user)
@@ -36,10 +40,13 @@ func Posts(c *gin.Context) {
 // Post returns a json result of a post by post_uuid
 func Post(c *gin.Context) {
 	uuidArg := c.Query("post_uuid")
+	if uuidArg == "" {
+		c.AbortWithError(400, errors.New("You need to request with a uuid query string"))
+	}
 	post := dbutils.Post{}
 	conversion, err := uuid.FromString(uuidArg)
 	if err != nil {
-		panic(err)
+		c.AbortWithError(400, errors.New("Invalid UUID"))	
 	}
 	dbutils.SelectPostsByUUID(conversion, &post)
 	c.JSON(200, post)
