0bf6a11bf4efa7690206bfdb2e5c7e67ce1ba7fc
gallo-json <gallojsantiago@gmail.com>
2020-08-16 17:59:18 -0500

    compiled sass

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function NavBar() {
     return (
         <div className="navbar">
-            <h1>Awty Portal</h1>       
-            <input className="search"></input>
+            <div>
+                <h1>Awty Portal</h1>   
+            </div>
+            <div> 
+                <input className="search"></input> 
+            </div>
         </div>
     );
 }
