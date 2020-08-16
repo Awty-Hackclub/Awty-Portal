eeb91d329c98d84cced5ee0abe6c60ffbefbf849
Tarek Ali <tarekali15@outlook.com>
2020-08-16 17:40:38 -0500

    added boilerplate jsx stuff

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,8 +3,9 @@ import React from 'react';
 function NavBar() {
     return (
         <div className="navbar">
-            
+            <h1>Awty Portal</h1>       
+            <input className="search"></input>
         </div>
-    )
+    );
 }
 
