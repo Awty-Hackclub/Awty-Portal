42f606373aa49f84da68fcc699599dc2dee9a4ff
Tarek Ali <tarekali15@outlook.com>
2020-08-17 16:49:02 -0500

    hyperlink instead of h1 for header

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './../styles/navbar.css'
 //import 'react-icons';
 
 function NavBar() {
     return (
         <div className="navbar">
-            <h1 class="nav-header">Awty Portal </h1>
+            <Link class="nav-header">Awty Portal </Link>
             <input className="searchBar"></input> 
         </div>
     )
