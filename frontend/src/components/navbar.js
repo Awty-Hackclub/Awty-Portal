5a427075c86a0d5576f6eaebe2d391e9376323cf
Tarek Ali <tarekali15@outlook.com>
2020-08-17 11:05:16 -0500

    nested styles

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,7 +3,7 @@ import './../styles/navbar.css'
 function NavBar() {
     return (
         <div className="navbar">
-            <h1>Awty Portal </h1>
+            <h1 class="nav-header">Awty Portal </h1>
             <input className="searchBar"></input> 
         </div>
     )
