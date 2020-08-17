6a67277b5be9a6c4b51bdf58f5d34ed8c2acfc38
gallo-json <gallojsantiago@gmail.com>
2020-08-17 17:08:24 -0500

    handle enter from searchbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './../styles/navbar.css'
 //import 'react-icons';
+// Doesn't work ^
 
 function NavBar() {
+    const handleEnter=(event) => {
+        if (event.keyCode === 13) {
+            // Handle enter
+            console.log('enter')
+        }
+    }
     return (
         <div className="navbar">
             <h1 class="nav-header" href="/">Awty Portal </h1>
-            <input className="searchBar"></input> 
+            <input className="searchBar" onKeyDown={(e) => handleEnter(e)}></input>
+            <button className="home" onClick={() => console.log("home button clicked")} icon="refresh"> </button>
         </div>
     )
 }
